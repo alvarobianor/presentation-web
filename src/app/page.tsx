8e2fc92c6cec469ece4a029bee7d3064fce3a71d
Álvaro Bianor <bianor.alvaro12@gmail.com>
2025-02-18 21:16:33 -0300

    Just training

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <main>
-      <div>Hello world!</div>
+      <h1 className="text-3xl text-green-500 h-[580px] bg-amber-700 mx-3 font-heading">
+        Hello world!
+      </h1>
     </main>
   );
 }
